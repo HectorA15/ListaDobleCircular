31ea96f7aa1afd84754d0a7f22bbc75fbc9dd93c
Angelsol2 <204301828+Angelsol2@users.noreply.github.com>
2025-10-30 02:08:52 -0600

    Refactor main method for song list operations
    Updated the main method to insert new songs and display list operations.

--- src/listacircula/ListaCircula.java
+++ src/listacircula/ListaCircula.java
@@ -6,14 +6,41 @@ public class ListaCircula {
 
     public static void main(String[] args) {
         
+   lista.insertarAlFrente(new Cancion("One more hour", "Tame Impala", 160));
+        lista.insertarAlFrente(new Cancion("A", "A,A,A", 130));
+        lista.insertarAlFrente(new Cancion("B ", "B,B,B", 180));
+        lista.insertarAlFrente(new Cancion("C", "C,C,C", 210));
+             lista.insertarAlFrente(new Cancion("D", "D,D,D", 210));
+        System.out.println("Tamano inicial " + lista.tamanio());
+        System.out.println();
         
-        ListaCircular lista = new ListaCircular();
-        lista.insertarAlFrente(new Cancion("One more hour", "Tame Impala", 160));
-        lista.insertarAlFrente(new Cancion("b b", "b", 130));
-        lista.insertarAlFrente(new Cancion("c c", "c c", 180));
-        lista.insertarAlFrente(new Cancion("d", "d d d", 210));
 
-       
+        System.out.println("Lista de Canciones");
+        for (Cancion c : lista.toList()) {
+           System.out.println(c);
+        }
+        System.out.println();
+
+        System.out.println("la navegacion del papu");
+          System.out.println();
+        System.out.println("Cancion actual: " + lista.getActual());
+        lista.next();
+        System.out.println("Despues de next(): " + lista.getActual());
+        lista.previous();
+        System.out.println("Despues de previous(): " + lista.getActual());
+
+   System.out.println();
+   
+        System.out.println("la eliminacion");
+        System.out.println("Eliminado de la cancion actual: " + lista.getActual());
+        lista.eliminarActual();
+
+        System.out.println("despues de eliminar:");
+        for (Cancion cancion : lista.toList()) {
+            System.out.println(cancion);
+        }
+          System.out.println();
+        System.out.println("tamano Final: " + lista.tamanio());
     }
 
 }
